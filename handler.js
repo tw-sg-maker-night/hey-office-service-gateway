1a615d1903716d993c181feab4f03773b4441676
Albert Salim <asalim@thoughtworks.com>
2017-01-07 14:38:28 +0800

    Handle API.AI
    - add API.AI response template
    - identify API.AI action

--- handler.js
+++ handler.js
@@ -1,16 +1,29 @@
 'use strict';
 
+const createApiAiResponse = (params) => {
+  const baseResponse = {
+    speech: 'Yes how can I help?',
+    displayText: 'Yes how can I help?',
+    data: {},
+    contextOut: [],
+    source: 'Somewhere only we know'
+  };
+  return Object.assign(baseResponse, params);
+};
+
 module.exports.route = (event, context, callback) => {
+  const body = JSON.parse(event.body);
+  const action = body.result.action;
+
+  const serverlessResponse = {
+    speech: `You asked for ${action} action`,
+    displayText: `You asked for ${action} action`
+  };
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    body: JSON.stringify(createApiAiResponse(serverlessResponse))
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
